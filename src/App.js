4a6dc45d4ea562026d4de53734cc04f88abcd437
CodingCocho <raulujr@gmail.com>
2021-12-26 22:28:45 -0800

    Added stack-box

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 function App() {
   
-  const [stack, setStack] = useState(new Stack())
+  const [stack, setStack] = useState(new Stack(5))
 
   return (
     <div className="App">
@@ -16,7 +16,11 @@ function App() {
 
         <div className='stack-container'>
           <div className='stack'>
-
+            <div className='stack-box'>1</div>
+            <div className='stack-box'>2</div>
+            <div className='stack-box'>3</div>
+            <div className='stack-box'>4</div>
+            <div className='stack-box'>5</div>
           </div>
         </div>
         
