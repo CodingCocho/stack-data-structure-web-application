5d230fce58162b96a42103b9750b3924c3a18477
CodingCocho <raulujr@gmail.com>
2021-12-27 22:14:44 -0800

    Finished functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
 import Stack from './Stack'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+/*
+This React.js App will run the frontend of the Stack data structure.
+*/
 function App() {
   
+  // Holds our useStates
+  // An useState for the Stack to keep track of the data structure 
   const [stack, setStack] = useState(new Stack(5))
+  // An useState for the pointer to keep track of when the page needs to render
+  const [pointer, setPointer] = useState(stack.getPointer())
+
+  // An useEffect the notifies the console of a render which is depended on the 
+  // pointer changing
+  useEffect(() => {
+    console.log("render");
+  }, [pointer])
+  
+  // Local function to set the new Stack 
+  const newStack = () =>
+  {
+    setStack(new Stack(5));
+  }
+
+  const pushStack = () =>
+  {
+    const element = prompt("Enter an element to push to the Stack", 0)
+    stack.push(element);
+    setPointer(stack.getPointer())
+  }
+
+  const popStack = () =>
+  {
+    stack.pop();
+    setPointer(stack.getPointer())
+  }
 
   return (
     <div className="App">
@@ -16,18 +48,21 @@ function App() {
 
         <div className='stack-container'>
           <div className='stack'>
-            <div className='stack-box'>1</div>
-            <div className='stack-box'>2</div>
-            <div className='stack-box'>3</div>
-            <div className='stack-box'>4</div>
-            <div className='stack-box'>5</div>
+            {stack.getStack().map( (element) =>
+            {
+              if(element === undefined)
+              {
+                return <div></div>
+              }
+              return <div className='stack-box'>{element}</div>
+            })}
           </div>
         </div>
         
         <div className='stack-functions-container'>
-          <button>New Stack</button>
-          <button>Push</button>
-          <button>Pop</button>
+          <button onClick={newStack}>New Stack</button>
+          <button onClick={pushStack}>Push</button>
+          <button onClick={popStack}>Pop</button>
         </div>
     </div>
   );
